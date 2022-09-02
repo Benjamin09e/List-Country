4c0c3ae06ae32c0529eec03c840b6b84008ccef9
EKIA Benjamin <ekiabenjamin@gmail.com>
2022-09-02 13:34:19 +0200

    liste des pays fait en react mais aussi du bootstrap pour la forme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import './App.css'
 
-function App() {
+
+const App = () => {
+
+  const [liste, setListe] = useState("")
+
+  const [valeur, setValeur] = useState("")
+
+  const [select, setSelect] = useState("")
+
+  const filtre =({currentTarget}) =>{
+    setValeur(currentTarget.value)
+    setListe(countryList.filter(word => word.toLowerCase().includes(currentTarget.value)))
+
+  }
+
+  const countryList = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas (the)",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia (Plurinational State of)",
+    "Bonaire, Sint Eustatius and Saba",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Bouvet Island",
+    "Brazil",
+    "British Indian Ocean Territo"]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="background d-flex justify-content-center" >
+        <div className= "form col-4 p-3 m-2" >
+         <p>Type the name of a country to search</p> 
+          <Form.Control className="mb-5" type="text" onChange={filtre} id="inputText" aria-describedby="textdHelpBlock"/>
+          {
+            valeur ==""?(<div></div>):(
+              
+            <div className="border">
+              <h1>Showing {liste.length} of {countryList.length} </h1>
+              <hr />
+                <div className="plus"> {liste.map(liste =>(
+                <p key={liste}>{liste}</p>
+               ))}
+                </div>
+            </div>
+             
+            
+          
+            )
+          }
+          
+        
+        </div >
+      </div>
+  )
 }
 
-export default App;
+export default App
